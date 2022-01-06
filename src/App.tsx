c2c1d9c6f4a9d93628e4382d7aec07c55371dbc8
Ricardo <pauloricardo_wow@hotmail.com>
2022-01-06 01:16:06 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Dashboard } from './components/Dashboard'
+import { Header } from './components/Header'
+import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+import { NewTransactionModal } from './components/NewTransactionModal'
+import { TransactionsProvider } from './TransactionsContext'
 
-function App() {
+Modal.setAppElement('#root') // Acessibilidade
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransctionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransctionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransctionModalOpen(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />{' '}
+      {/* Passando a propriedade onOpenNewTransactionModal para o Header. Pai para filho. Onde o filho modifica funcionalidades do pai */}
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </TransactionsProvider>
+  )
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createServer, Model } from 'miragejs'
+import { App } from './App'
+
+createServer({
+  models: {
+    transaction: Model // "Banco de dados" com a primeira tabela
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00')
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transaction', () => {
+      return this.schema.all('transaction') // Método get, retorna todas as transactions. schema é o banco de dados
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody) // recebe as informações em JSON
+
+      return schema.create('transactions', data) // Cria as informações no banco de dados. Link entre o get e o post
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
